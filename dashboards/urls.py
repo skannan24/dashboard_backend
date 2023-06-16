b0499be4a744b4bfea499bc4d9b26a841f35d925
skannan24 <sundar.kannan@leanagri.com>
2023-06-16 15:20:33 +0530

    refactor(dashboard): add default route

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -8,6 +8,6 @@ router.register('dashboards', views.DashboardsAPI)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', include(router.urls)),
     path('api/', include(router.urls))
 ]
