10006693d43223a30e633c3265094b2659aa9530
skannan24 <sundar.kannan@leanagri.com>
2023-06-16 17:56:34 +0530

    refactor(dashboard): change allow host

--- dashboards/settings.py
+++ dashboards/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2$ncffsl5pm2()c(m6ju3q-+6y)^k+u0h@v*!!&k&57_m15y#e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
